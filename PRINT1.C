ee0b045fd5995ab74c234ee85fe53daea87b6ea6
Yongzan <yongzanlu@gmail.com>
2017-04-06 15:19:51 +0800

    print 3 version

--- PRINT1.C
+++ PRINT1.C
@@ -1,35 +1,62 @@
-/*???????????????????????????????????????????
-PRINT2.C ?? Printing with Abort Procedure
+/*???????????????????????????????????????
+PRINT3.C ?? Printing with Dialog Box
 (c) Charles Petzold, 1998
-???????????????????????????????????????????*/
+???????????????????????????????????????*/
 #include <windows.h>
 
 HDC GetPrinterDC (void) ; // in GETPRNDC.C
 void PageGDICalls (HDC, int, int) ; // in PRINT.C
 
 HINSTANCE hInst ;
-TCHAR szAppName[] = TEXT ("Print2") ;
-TCHAR szCaption[] = TEXT ("Print Program 2 (Abort Procedure)") ;
+TCHAR szAppName[] = TEXT ("Print3") ;
+TCHAR szCaption[] = TEXT ("Print Program 3 (Dialog Box)") ;
+
+BOOL bUserAbort ;
+HWND hDlgPrint ;
+
+
+BOOL CALLBACK PrintDlgProc (HWND hDlg, UINT message,
+                            WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_INITDIALOG:
+         SetWindowText (hDlg, szAppName) ;
+         EnableMenuItem (GetSystemMenu (hDlg, FALSE), SC_CLOSE, MF_GRAYED) ;
+         return TRUE ;
+
+	case WM_COMMAND:
+         bUserAbort = TRUE ;
+         EnableWindow (GetParent (hDlg), TRUE) ;
+         DestroyWindow (hDlg) ;
+         hDlgPrint = NULL ;
+         return TRUE ;
+	}
+    return FALSE ;
+}
+
 
 BOOL CALLBACK AbortProc (HDC hdcPrn, int iCode)
 {
 	MSG msg ;
-
-	while (PeekMessage (&msg, NULL, 0, 0, PM_REMOVE))
+	while (!bUserAbort && PeekMessage (&msg, NULL, 0, 0, PM_REMOVE))
 	{
-		TranslateMessage (&msg) ;
-		DispatchMessage (&msg) ;
+		if (!hDlgPrint || !IsDialogMessage (hDlgPrint, &msg))
+		{
+			TranslateMessage (&msg) ;
+			DispatchMessage (&msg) ;
+		}
 	}
-	return TRUE ;
+	return !bUserAbort ;
 }
 
 
 BOOL PrintMyPage (HWND hwnd)
 {
-	static DOCINFO di = { sizeof (DOCINFO), TEXT ("Print2: Printing") } ;
+	static DOCINFO di = { sizeof (DOCINFO), TEXT ("Print3: Printing") } ;
 	BOOL bSuccess = TRUE ;
 	HDC hdcPrn ;
-	short xPage, yPage ;
+	int xPage, yPage ;
 
 	if (NULL == (hdcPrn = GetPrinterDC ()))
 		return FALSE ;
@@ -39,6 +66,10 @@ BOOL PrintMyPage (HWND hwnd)
 
 	EnableWindow (hwnd, FALSE) ;
 
+	bUserAbort = FALSE ;
+	hDlgPrint = CreateDialog (hInst, TEXT ("PrintDlgBox"),
+                              hwnd, PrintDlgProc) ;
+
 	SetAbortProc (hdcPrn, AbortProc) ;
 
 	if (StartDoc (hdcPrn, &di) > 0)
@@ -46,16 +77,23 @@ BOOL PrintMyPage (HWND hwnd)
 		if (StartPage (hdcPrn) > 0)
 		{
 			PageGDICalls (hdcPrn, xPage, yPage) ;
-            if (EndPage (hdcPrn) > 0)
+
+			if (EndPage (hdcPrn) > 0)
 				EndDoc (hdcPrn) ;
-            else
-                bSuccess = FALSE ;
+			else
+				bSuccess = FALSE ;
 		}
 	}
 	else
 		bSuccess = FALSE ;
 
-	EnableWindow (hwnd, TRUE) ;
+	if (!bUserAbort)
+	{
+		EnableWindow (hwnd, TRUE) ;
+		DestroyWindow (hDlgPrint) ;
+	}
+
 	DeleteDC (hdcPrn) ;
-	return bSuccess ;
+
+	return bSuccess && !bUserAbort ;
 }
